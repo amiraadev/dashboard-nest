070764efade68f9b5dacd6cebc05120cc398416f
allagui_fawzi@outlook.fr <allagui_fawzi@outlook.fr>
2023-12-10 22:24:22 +0100

    prisma global

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signupLocal() {}
   signinLocal() {}
   logout() {}
