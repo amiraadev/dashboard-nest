324884fdf7ceea8a11a1eb6d67b77e44c106602a
allagui_fawzi@outlook.fr <allagui_fawzi@outlook.fr>
2023-12-11 00:05:34 +0100

    11

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
   async signupLocal(dto: AuthDto) {
     const existingUser = await this.prisma.user.findUnique({
       where: {
@@ -17,9 +21,13 @@ export class AuthService {
         'This email is already in use. Please choose another.',
       );
     }
+    const hash = await this.hashData(dto.password);
 
     const createdUser = await this.prisma.user.create({
-      data: { ...dto },
+      data: {
+        email: dto.email,
+        hash: hash,
+      },
     });
     return createdUser;
   }
