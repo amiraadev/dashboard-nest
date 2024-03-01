7365d4229cf4d24ea298ecb9594d18339902d660
amiraadev <allagui_fawzi@outlook.fr>
2024-03-01 02:25:21 +0100

    files.module.ts

--- backend/src/files/files.module.ts
+++ backend/src/files/files.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports:[
+MulterModule.register({
+  dest:'./uploads'
+})
+  ],
   controllers: [FilesController],
   providers: [FilesService]
 })
