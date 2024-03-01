24127838e8ab43b569bda0674826fa135b618abf
amiraadev <allagui_fawzi@outlook.fr>
2024-03-01 02:33:50 +0100

    Upload file using Nestjs and multer Nestjs

--- backend/src/files/files.controller.ts
+++ backend/src/files/files.controller.ts
@@ -5,12 +5,17 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from "multer";
 
 @Controller('files')
 export class FilesController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    if(!file){
+      throw new Error('No file to upload')
+    }
+    console.log('File uploaded',file);
+    return {message:"file uploaded successfully"}
   }
 }
