90005c2904d2903d3d2d211b0092b449e58e7e23
amiraadev <allagui_fawzi@outlook.fr>
2024-03-02 02:09:05 +0100

    Added API endpoint for add/remove friend based on their unique ID and the friend ID : it worked for the add , still have to check the removal of the friend

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -11,7 +11,7 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { Friend, User, UserDto } from './dto/user.dto';
+import { Friend, User } from './dto/user.dto';
 import { AtGuard } from 'src/common/decorators/guards';
 
 @Controller('user')
@@ -30,29 +30,12 @@ export class UserController {
   getUserFriends(@Param('id') id: string):  Promise<Friend[]> {
     return this.userService.getUserFriends(id);
   }
-
-  @Post()
-  create(@Body() createUserDto: UserDto) {
-    return this.userService.create(createUserDto);
+  @UseGuards(AtGuard)
+  @Post('/:id')
+  @HttpCode(HttpStatus.OK)
+  addRemoveFriend(@Param('id') id: string,@Body('friendId') friendId:string):  Promise<Friend> {
+    return this.userService.addRemoveFriend(id,friendId);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
+  
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,17 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Friend, User, UserDto } from './dto/user.dto';
+import { Friend, User } from './dto/user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
- 
+
   async getUserById(id: string): Promise<User> {
     const user = await this.prisma.user.findUnique({
       where: { id },
     });
     if (!user) {
-      throw new NotFoundException("User not found");
+      throw new NotFoundException('User not found');
     }
     return {
       id: user.id,
@@ -26,34 +26,77 @@ export class UserService {
     };
   }
 
-  
   async getUserFriends(id: string): Promise<Friend[]> {
-    const friends = await this.prisma.user.findUnique({
-      where: { id: id },
-    }).friends();
-    if (!friends) {
-      throw new NotFoundException("User not found");
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+
+    const friends = await this.prisma.user
+      .findUnique({
+        where: { id: id },
+      })
+      .friends();
+
     return friends;
   }
 
-  create(createUserDto: UserDto) {
-    return createUserDto;
+  
+  async addRemoveFriend(id: string, friendId: string): Promise<Friend> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const friend = await this.prisma.user.findUnique({
+      where: { id: friendId },
+    });
+    if (!friend) {
+      throw new NotFoundException(
+        'there is no such a friend subscribed in this platform!',
+      );
+    }
+    const userFriends = await this.prisma.user
+      .findUnique({
+        where: { id: id },
+      })
+      .friends();
+
+    const friendExist = userFriends.find((frd) => {
+      frd.id === friendId;
+    });
+    if (!friendExist) {
+      const friendToAdd = {
+        id: friend.id,
+        userId: user.id,
+        firstName: friend.firstName,
+        lastName: friend.lastName,
+        picturePath: friend.picturePath,
+        occupation: friend.occupation,
+        location: friend.location,
+      };
+      const addedFriend = await this.prisma.friend.create({
+        data:friendToAdd
+      })
+      console.log(
+        `${addedFriend.firstName} ${addedFriend.lastName} has been added to the friend's list of ${user.firstName} ${user.lastName}`,
+      );
+      return addedFriend;
+    }
+    const removedFriend = await this.prisma.friend.delete({
+      where: {
+        id: friendId,
+        userId: id,
+      },
+    });
+    console.log(
+      `${removedFriend.firstName} ${removedFriend.lastName} has been removed from the friend's list of ${user.firstName} ${user.lastName}`,
+    );
+    return removedFriend;
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  
 }
