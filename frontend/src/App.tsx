714edd4e643c342c3f63217fa911a6b94f7c31b0
amiraadev <allagui_fawzi@outlook.fr>
2024-03-10 10:41:12 +0100

    configure the persistedReducer in the main.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
 import { BrowserRouter,Navigate ,Routes,Route } from 'react-router-dom'
-import HomePage from 'scenes/homePage'
+import HomePage from './scenes/homePage'
+import LoginPage from './scenes/loginPage'
+import ProfilePage from './scenes/profilePage'
 
-HomePage
 function App() {
 
   return (
     <div className='app'>
-    
+     <BrowserRouter >
+     <Routes>
+      <Route path="/" element={<LoginPage/>}/>
+      <Route path="/home" element={<HomePage/>}/>
+      <Route path="/profile/:userId" element={<ProfilePage/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   )
 }
---
+++ frontend/src/state/index.ts
@@ -0,0 +1,69 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Interfaces for type definitions
+interface User {
+    friends: string[];
+}
+
+interface Post {
+    id: string;
+    userId: string;
+    title: string;
+    description: string;
+    location: string;
+    userpicturePath: string;
+    picturePath: string;
+    postPictures: string[];
+    PostUserPicture: string; // Consider renaming to match other property casings
+}
+
+// Initial state with types
+const initialState: {
+    mode: string;
+    user: User | null;
+    token: string | null;
+    posts: Post[];
+} = {
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setMode(state) {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin(state, action) {
+            state.user = action.payload.user as User; 
+            state.token = action.payload.token as string; 
+        },
+        setLogout(state) {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends(state, action) {
+            if (state.user) {
+                state.user.friends = action.payload.friends as string[]; 
+            } else {
+                console.log("user friends not found");
+            }
+        },
+        setPosts(state, action) {
+            state.posts = action.payload.posts as Post[]; 
+        },
+        setPost(state, action) {
+            const updatedPosts = state.posts.map((post)=>{
+                if(post.id === action.payload.postId) return action.payload.post;
+                return post
+            })
+          state.posts = updatedPosts
+        },
+    },
+});
+
+export const {setMode,setLogin,setLogout,setFriends,setPosts,setPost} = authSlice.actions
+export default authSlice.reducer
