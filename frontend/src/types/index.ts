fd37f3fdb7b707c5d7394cca12b70034fca50e1a
amiraadev <allagui_fawzi@outlook.fr>
2024-03-10 20:17:58 +0100

    login form

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,29 +1,41 @@
+/** @format */
+
 export interface User {
-    firstName: string;
-    lastName: string;
-    email: string;
-    picturePath?: string;
-    location?: string;
-    occupation?: string;
-    viewedProfile:number;
-    impressions:number;
-    friends: string[];
+	firstName: string;
+	lastName: string;
+	email: string;
+	picturePath?: string;
+	location?: string;
+	occupation?: string;
+	viewedProfile: number;
+	impressions: number;
+	friends: string[];
 }
 
 export interface Post {
-    id: string;
-    userId: string;
-    title: string;
-    description: string;
-    location: string;
-    userpicturePath: string;
-    picturePath: string;
-    postPictures: string[];
-    PostUserPicture: string; 
+	id: string;
+	userId: string;
+	title: string;
+	description: string;
+	location: string;
+	userpicturePath: string;
+	picturePath: string;
+	postPictures: string[];
+	PostUserPicture: string;
 }
 export interface StateProps {
-    mode: string;
-    user: User | null;
-    token: string | null;
-    posts: Post[]; 
-  }
+	mode: string;
+	user: User | null;
+	token: string | null;
+	posts: Post[];
+}
+
+export interface RegisterProps {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+	location: string;
+	occupation: string;
+	picture: string;
+}
